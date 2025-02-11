90a5b9aab9607f33dedd14fdf1b3a89f12bcd44a
Alex <alexander.reis84@gmail.com>
2025-02-11 15:09:17 +0100

    Update Spieltag 21

--- import_kicker_data.py
+++ import_kicker_data.py
@@ -2,12 +2,12 @@ import sqlite3
 import csv
 
 DB_PATH = "kicker-data.sqlite"
-CSV_PATH = "autodownload/data_2025-02-05_16-17-03.csv"  # CSV während der Transferphase
+CSV_PATH = "autodownload/data_2025-02-10_10-17-03.csv"  # CSV während der Transferphase
 
 # Datum und Spieltag (nur wenn es tatsächlich einen neuen Spieltag gibt)
 # Wenn Sie noch nicht wissen, ob Sie einen neuen Spieltag anlegen, können Sie diesen Wert dynamisch festlegen oder weglassen
-SPIELTAG_NUM = 20
-SPIELTAG_DATUM = "2025-02-02 23:59:59"  # Beispiel
+SPIELTAG_NUM = 21
+SPIELTAG_DATUM = "2025-02-09 23:59:59"  # Beispiel
 
 conn = sqlite3.connect(DB_PATH)
 cur = conn.cursor()
